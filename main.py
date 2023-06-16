d4fb4eb48293814eafe0e42dfe6d573d6ed84984
ctdub <connorterryweatherly2025@u.northwestern.edu>
2023-06-16 10:01:24 -0500

    6-16-23: Completed twa model and started the Data notebook

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import twa_model as twa
+import matplotlib.pyplot as plt
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+k2eV = 4.55633e-6
+parameters = {'cavity freq': 1600 * k2eV,  # (cm-1) -> (A.U.)
+              'vibration freq': 1600 * k2eV,  # (cm-1) -> (A.U.)
+              'Huang-Rhys': 1,
+              'coupling': 0.04e-3,  # (A.U)
+              'final time': 10,  # (ps)
+              'time steps': 4000,
+              'field size': 500,
+              'bath size': 500,
+              'res range': 878 * k2eV,  # (cm-1) -> (A.U.)
+              'vibration lifetime': 2,  # (ps)
+              'Q': 1600  #  Cavity quality factor
+              }
 
+polEn, polStates, siteEn = twa.polariton_modes(parameters)
+polDispl = twa.pol_displacement(parameters, polEn, polStates, siteEn)
+nCav = twa.oc_dynamics(parameters, polEn, polStates, siteEn, polDispl, subsystem='cavity')
+nVib = twa.oc_dynamics(parameters, polEn, polStates, siteEn, polDispl, subsystem='vibration')
+nField = twa.oc_dynamics(parameters, polEn, polStates, siteEn, polDispl, subsystem='field')
+nBath = twa.oc_dynamics(parameters, polEn, polStates, siteEn, polDispl, subsystem='bath')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.plot(nCav[0], nCav[1], label='cavity', color='k')
+plt.plot(nVib[0], nVib[1], label='vibration', color='tab:blue')
+plt.plot(nField[0], nField[1], label='external field', color='tab:orange')
+plt.plot(nBath[0], nBath[1], label='non-radiative bath', color='tab:green')
+plt.gca().spines['left'].set_linewidth(2)
+plt.gca().spines['bottom'].set_linewidth(2)
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
+plt.xlabel('time / ps', fontsize=20)
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=16)
+plt.ylabel(r'$\langle N_{x} \rangle$', fontsize=20)
+plt.xticks(fontsize=16)
+plt.gca().xaxis.set_tick_params(width=2)
+plt.gca().yaxis.set_tick_params(width=2)
+legend = plt.legend(title='$x = $', loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, fontsize=16)
+legend.get_title().set_fontsize('20')
+legend.get_frame().set_edgecolor('black')
--- twa_model.py
+++ twa_model.py
@@ -1,31 +1,84 @@
 import numpy as np
 from numpy import linalg as LA
 
-
-def polariton_modes(param):
-    hessian = np.zeros((2, 2))
-    hessian[0, 0] = param['cavity freq'] ** 2
-    hessian[1, 1] = param['vibration freq'] ** 2
-    G = 2 * np.sqrt(param['cavity freq'] * param['vibration freq']) * param['coupling']
-    hessian[1, 0] = G
-    hessian[0, 1] = G
-    return LA.eigh(hessian)
+k2eV = 4.55633e-6
+c = 2.998e10  # speed of light in cm/s
 
 
-def occ_dynamics(param, v, u):
-    energies = np.array([param['cavity freq'], param['vibration freq']]) ** 2
-    displ = np.zeros(2)
-    displ[1] = np.sqrt(2 * param['Huang-Rhys'] / param['vibration freq'])  # displacements along vibrational mode
-    polDispl = u.T.dot(energies * displ) / v
+def polariton_modes(pr):
+    dF = pr['res range'] / 2
+    field_en = np.linspace(pr['cavity freq'] - dF, pr['cavity freq'] + dF, pr['field size']) ** 2
+    bath_en = np.linspace(pr['vibration freq'] - dF, pr['vibration freq'] + dF, pr['bath size']) ** 2
 
-    time = np.linspace(0, param['final time'], param['time steps'])
+    fDamp = 1 / (2 * np.pi * pr['Q'])
+    kappa = np.sqrt(fDamp * np.sqrt(field_en) * 2 * dF / pr['field size'])
+    fieldG = 2 * np.sqrt(pr['cavity freq'] * np.sqrt(field_en)) * kappa
+    fieldG = np.append(fieldG, np.zeros(pr['bath size']))
+
+    omegaCHz = 2 * np.pi * c * pr['cavity freq'] / k2eV
+    QBath = omegaCHz * pr['vibration lifetime'] * 1e-12
+    bDamp = 1 / (2 * np.pi * QBath)
+    chi = np.sqrt(bDamp * np.sqrt(bath_en) * 2 * dF / pr['bath size'])
+    bathG = 2 * np.sqrt(pr['vibration freq'] * np.sqrt(bath_en)) * chi
+
+    sysG = 2 * np.sqrt(pr['cavity freq'] * pr['vibration freq']) * pr['coupling']
+    cavG = np.append(sysG, fieldG)
+
+    energies = np.append([pr['cavity freq'] ** 2, pr['vibration freq'] ** 2], np.append(field_en, bath_en))
+    hessian = np.diag(energies)
+    hessian[1:, 0] = cavG
+    hessian[0, 1:] = cavG
+    hessian[2 + pr['field size']:, 1] = bathG
+    hessian[1, 2 + pr['field size']:] = bathG
+    v, u = LA.eigh(hessian)
+    return v, u, energies
+
+
+def pol_displacement(pr, v, u, energies):
+    totalSize = v.size
+    displ = np.zeros(totalSize)
+    displ[1] = np.sqrt(2 * pr['Huang-Rhys'] / pr['vibration freq'])  # displacement along the vibrational mode
+    return u.T.dot(energies * displ) / v
+
+
+def oc_dynamics(pr, v, u, energies, polDispl, subsystem='cavity'):
+    time = np.linspace(0, pr['final time'], pr['time steps'])
     timeAU = time / 2.41889E-5
-    omega_t = np.tile(timeAU, (2, 1)).T * np.sqrt(v)
-    t1 = np.sum(u[0, :] * polDispl * np.sqrt(v) * np.sin(omega_t), axis=1)
-    t2 = np.sum(u[0, :] * polDispl * (np.sin(omega_t) ** 2), axis=1)
+    totalSize = v.size
+    omega_t = np.tile(timeAU, (totalSize, 1)).T * np.sqrt(v)
 
-    ncav_t = 1 / (2 * param['cavity freq']) * np.square(t1) + 2 * param['cavity freq'] * np.square(t2)
-    ncavBeta = 1 / param['cavity freq'] * np.square(u[0, :]).dot(np.sqrt(v)) + param['cavity freq'] * np.square(
-        u[0, :]).dot(1 / np.sqrt(v))
-    return ncav_t + 0.25 * ncavBeta - 0.5
+    subSize = 1
+    if subsystem == 'cavity':
+        start = 0
+        stop = 1
+    if subsystem == 'vibration':
+        start = 1
+        stop = 2
+    if subsystem == 'field':
+        start = 2
+        stop = pr['field size'] + 2
+        subSize = pr['field size']
+    if subsystem == 'bath':
+        start = pr['bath size'] + 2
+        stop = pr['bath size'] + pr['field size'] + 2
+        subSize = pr['bath size']
+    if subsystem not in {'cavity', 'vibration', 'field', 'bath'}:
+        raise ValueError('invalid subsystem name')
+    if subSize == 0:
+        proj = np.zeros(v.size)[np.newaxis, :]
+        subEn = [1]
+    else:
+        proj = u[start:stop, :]
+        subEn = energies[start:stop]
 
+    P = np.einsum('ij,kj', proj, polDispl * np.sqrt(v) * np.sin(omega_t)).T
+    Q = np.einsum('ij,kj', proj, polDispl * (np.sin(omega_t / 2) ** 2)).T
+
+    n_t = np.sum(1 / (2 * np.sqrt(subEn)) * np.square(P) + 2 * np.sqrt(subEn) * np.square(Q), axis=1)
+    nBeta = np.sum(
+        1 / np.sqrt(subEn) * np.square(proj).dot(np.sqrt(v)) + np.sqrt(subEn) * np.square(proj).dot(1 / np.sqrt(v)))
+
+    n_t2 = 0
+    if subsystem == 'vibration':
+        n_t2 = - 2 * np.sqrt(2 * pr['Huang-Rhys'] * pr['vibration freq']) * np.sum(Q, axis=1) + pr['Huang-Rhys']
+    return [time, n_t + n_t2 + 0.25 * nBeta - 0.5 * subSize]
